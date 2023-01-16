38fb490ee04736af781f22ecf8006b5c34cb4420
Even Sletteng Garvang <evengar@uio.no>
2023-01-16 14:59:05 +0100

    added functions for reading raw trios data

---
+++ R/trios_process.R
@@ -0,0 +1,53 @@
+#' Title
+#'
+#' @param path 
+#' @param depths 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+trios_process <- function(path, depths){
+  
+  f <- dir(path)
+  # remove any calibration files that may be present
+  f <- f[!grepl("CAL", f)]
+  
+  # sensor ID comes after "SAM_" or "SAMIP_"
+  sensor <- factor(gsub(".*SAM(IP)?_(.{4})_.*", "\\2", f))
+  # sensor_table = table(sensor)
+  # stopifnot(all(sensor_table[1]==sensor_table[2:length(sensor_table)]))
+
+  # Get date and time by pattern matching
+  dt_string <- gsub(".*_([0-9]{4}-[0-9]{2}-[0-9]{2}_[^_]*)_.*", "\\1", f)
+  time <- as.POSIXct(strptime(dt_string, "%Y-%m-%d_%H-%M-%S"))
+  
+  ff <- data.frame(f, sensor, time)
+  ff <- ff[order(sensor, time), ]
+  ff$depth <- rep(depths, 3)
+  
+  # Aggregate all files into one big table
+  trios <- NULL
+  for (i in (1:length(f))) {
+    # Concatenate path + file name
+    fi <- paste(path, ff$f[i], sep = "/")
+    
+    # Read all lines as strings (no tabs in file)
+    di <- read.table(fi, sep = "\t", as.is = TRUE)$V1
+    
+    # Use occurrences of "DATA" to find
+    # beginning and end of data part of the file
+    k <- grep("DATA", di)
+    di <- di[(k[1]+2):(k[2]-1)]
+    
+    # Split data strings and extract wavelength and power
+    mi <- matrix(unlist(strsplit(di, " ")), nrow = 5)
+    wi <- as.numeric(mi[2,])
+    Pi <- as.numeric(mi[3,])
+    
+    # Add to aggregated data table
+    trios <- rbind(trios,
+                   data.frame(sensor = ff$sensor[i], depth = ff$depth[i], wavelength = wi, power = Pi))
+  }
+  return(trios)
+}
---
+++ R/trios_sync_sensors.R
@@ -0,0 +1,52 @@
+#' Title
+#'
+#' @param trios_long
+#' @param sensor_air 
+#' @param sensor_up 
+#' @param sensor_down 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+trios_sync_sensors <- function(trios_long, 
+                               sensor_air = "8175",
+                               sensor_up = "8078",
+                               sensor_down = "501A",
+                               sensor_down_alt = "817c"
+                                 ){
+  
+  d.air  <- subset(trios_long, sensor == sensor_air)
+  d.up   <- subset(trios_long, sensor == sensor_up)
+  d.down <- subset(trios_long, sensor == sensor_down | sensor == sensor_down_alt)
+  
+  # Make separate wavelength by depth matrices for the 3 sensors 
+  # (air reference, water downwelling, and water upwelling)
+  
+  p.air <- tapply(d.air$power, list(wavelength=d.air$wavelength, depth=d.air$depth), sum)
+  w.air <- as.numeric(rownames(p.air))
+  # matplot(w.air, p.air, type="l", lty=1, col=topo.colors(10), lwd=3, main="Air", 
+  #         xlab="Wavelength (nm)", ylab="Power (?W / m2 / nm)", xlim=c(350, 750))
+  # 
+  p.down <- tapply(d.down$power, list(wavelength=d.down$wavelength, depth=d.down$depth), sum)
+  w.down <- as.numeric(rownames(p.down))
+  # matplot(w.down, p.down, type="l", lty=1, col=topo.colors(10), lwd=3, main="Downwelling",
+  #         xlab="Wavelength (nm)", ylab="Power (?W / m2 / nm)", xlim=c(350, 750))
+  # 
+  p.up <- tapply(d.up$power, list(wavelength=d.up$wavelength, depth=d.up$depth), sum)
+  w.up <- as.numeric(rownames(p.up))
+  # matplot(w.up, p.up, type="l", lty=1, col=topo.colors(10), lwd=3, main="Upwelling",
+  #         xlab="Wavelength (nm)", ylab="Power (?W / m2 / nm)", xlim=c(350, 750))
+  # 
+  # Interpolate all spectra to the same wavelengths
+  
+  w <- w.air
+  for (i in (1:10)) {
+    p.up[, i] <- approx(w.up, p.up[, i], w)$y
+    p.down[, i] <- approx(w.down, p.down[, i], w)$y
+  }
+  rownames(p.up) <- w
+  rownames(p.down) <- w
+  
+  return(list(p.air, p.up, p.down))
+}
