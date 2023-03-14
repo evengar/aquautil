17c1647972e60dfcf10cdb5b6dd7d1602d09129e
Even Sletteng Garvang <evengar@uio.no>
2023-03-14 12:20:45 +0100

    removed subsetting from functions altogether It's better to subset the object prior to plotting, rather than have limited subset options within the plotting and reflectance functions

--- R/trios_plot_reflectance.R
+++ R/trios_plot_reflectance.R
@@ -1,8 +1,6 @@
 #' @export
-trios_get_reflectance <- function(trios, par_region=TRUE){
-  if (par_region){
-    subset_to_wavelength(trios, 400, 700)
-  }
+trios_get_reflectance <- function(trios){
+
   
   p.refl <- trios[["up"]] / trios[["down"]] # calculate spectral reflectance
   
@@ -13,12 +11,11 @@ trios_get_reflectance <- function(trios, par_region=TRUE){
 
 #' @export
 trios_plot_reflectance <- function(trios){
-  w <- as.numeric(rownames(trios[[1]]))
-  w.par <- w[(w > 400) & (w < 700)]
-  p.refl.par <- trios_get_reflectance(trios)
+
+  p.refl <- trios_get_reflectance(trios)
   
   
   Rainbow <- colorRampPalette(c("violet", "blue1", "green", "yellow", "orange", "red", "darkred"))
-  plot(x = w.par, y = p.refl.par[,1], type="h", lwd=4, col=Rainbow(length(w.par)))
+  plot(x = trios$w, y = p.refl[,1], type="h", lwd=4, col=Rainbow(length(trios$w)))
   abline(v=665, lty=2)
 }
