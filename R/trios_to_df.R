718ae3c9e4798486c4ec5cb70b95b1636736aed6
Even Sletteng Garvang <evengar@uio.no>
2023-03-16 14:46:13 +0100

    Implemented function to convert trios to df

--- R/trios_to_df.R
+++ R/trios_to_df.R
@@ -1,3 +1,18 @@
+#' Convert a trios object to a data frame in long format
+#'
+#' @param trios 
+#'
+#' @return A data frame in long form with columns for depth, wavelength, sensor type and irradiance value
+#' @export
+#'
 trios_to_df <- function(trios){
   
+  with(trios, data.frame(depth=rep(depth, each=length(w)), 
+                         w=rep(w, length(depth)),
+                         sensor = factor(rep(c("up", "down", "air"), each = length(w) * length(depth))),
+                         irradiance = c(stack(as.data.frame(up))[,1],
+                                         stack(as.data.frame(down))[,1],
+                                         stack(as.data.frame(air))[,1])
+                       )
+     )
 }
