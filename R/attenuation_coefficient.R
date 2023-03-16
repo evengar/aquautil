8b8751de7c6a9f95e4b19f8c591ff02af9535ea1
Even Sletteng Garvang <evengar@uio.no>
2023-03-16 14:45:48 +0100

    implemented calculation of attenuation coefficient. Needs extensive testing.

--- R/attenuation_coefficient.R
+++ R/attenuation_coefficient.R
@@ -1,16 +1,20 @@
+#' @export
 attenuation_coefficient <- function(trios){
   
+  if (length(trios) == 2){
+    return(trios$up[,2] - trios$up[,1])
+  }
   
+  df <- trios_to_df(trios)
+  
+  df <- subset(df, sensor == "up")
   
   df$fw <- factor(df$w) # Need to have wavelength as factor
-  summary(m <- lmer(log(values) ~ z + (z | fw), data=df))
-  
-  plot(log(df$values), predict(m)) # Good fit :)
-  abline(c(0, 1), lty=2)
+  m <- lme4::lmer(log(irradiance) ~ depth + (depth | fw), data=df)
   
   # Slope for each wavelength = fixed effect + random effect
   # Negative slope is attenuation coefficient as function of wavelength
   
   wl <- as.numeric(rownames(ranef(m)$fw))
-  att <- -(fixef(m)[2] + ranef(m)$fw[, 2])
+  -(fixef(m)[2] + ranef(m)$fw[, 2])
 }
