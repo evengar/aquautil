cb76f14e66a6a62c31c12355eca4ec188a402886
Even Sletteng Garvang <evengar@uio.no>
2023-03-17 14:07:24 +0100

    renamed to fit naming scheme

---
+++ R/plot_reflectance.R
@@ -0,0 +1,21 @@
+#' @export
+get_reflectance <- function(trios){
+
+  
+  p.refl <- trios[["up"]] / trios[["down"]] # calculate spectral reflectance
+  
+  return(p.refl)
+}
+
+
+
+#' @export
+plot_reflectance <- function(trios){
+
+  p.refl <- get_reflectance(trios)
+  
+  
+  plot(x = trios$w, y = p.refl[,1], type="h", lwd=4, col=trios_palette(length(trios$w)),
+       main = "Surface reflectance")
+  abline(v=665, lty=2)
+}
