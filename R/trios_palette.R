213b1d9a212f1110d9a59dc553deab7b8c6e4882
Even Sletteng Garvang <evengar@uio.no>
2023-03-17 09:05:36 +0100

    moved palette to separate function

---
+++ R/trios_palette.R
@@ -0,0 +1,4 @@
+trios_palette <- function(n){
+  Rainbow <- colorRampPalette(c("violet", "blue1", "green", "yellow", "orange", "red", "darkred"))
+  Rainbow(n)
+}
--- R/trios_plot_reflectance.R
+++ R/trios_plot_reflectance.R
@@ -15,7 +15,6 @@ trios_plot_reflectance <- function(trios){
   p.refl <- trios_get_reflectance(trios)
   
   
-  Rainbow <- colorRampPalette(c("violet", "blue1", "green", "yellow", "orange", "red", "darkred"))
-  plot(x = trios$w, y = p.refl[,1], type="h", lwd=4, col=Rainbow(length(trios$w)))
+  plot(x = trios$w, y = p.refl[,1], type="h", lwd=4, col=trios_palette(length(trios$w)))
   abline(v=665, lty=2)
 }
