be869975f636368b7b396dcaaa3a97a680e71f0a
Even Sletteng Garvang <evengar@uio.no>
2023-03-17 12:00:32 +0100

    Created functions for plotting downwelling irradiance

---
+++ R/plot_downwelling_irradiance.R
@@ -0,0 +1,12 @@
+#' @export
+plot_downwelling_irradiance <- function(trios){
+  matplot(trios$w, 
+          trios$down, 
+          type = "l",
+          lty=1, 
+          col=topo.colors(length(trios)), 
+          lwd=3, 
+          main="Downwelling irradiance", 
+          xlab="Wavelength (nm)", 
+          ylab="Power (µW / m2 / nm)")
+}
