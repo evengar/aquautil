943186001dd8aab528a16a280ddf7459a37cbf19
Even Sletteng Garvang <evengar@uio.no>
2023-03-28 10:58:40 +0200

    added option to add title to plot frame

--- R/plot.R
+++ R/plot.R
@@ -9,13 +9,17 @@
 #' plot(trios_march)
 #' plot(trios_september)
 #' 
-plot.trios <- function(trios){
+plot.trios <- function(trios, title = NULL){
   par(mfrow = c(2,2))
   
   plot_depth_attenuation(trios)
   plot_downwelling_irradiance(trios)
   plot_attenuation_coefficient(trios)
   plot_reflectance(trios)
+  
+  if(!is.null(title)){
+    mtext(title, side = 3, line = -2, outer = TRUE)
+  }
   
   par(mfrow = c(1,1))
 }
